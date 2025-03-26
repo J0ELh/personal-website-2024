236c015f54fc9750945bff513ded246f2ee71c7a
Joel Hempel <joelhempel35@gmail.com>
2025-03-26 14:55:30 -0700

    modified package.json and config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,12 @@
-// next.config.mjs
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
   images: {
-    // For GH Pages, you often want no built-in Next image optimization
     unoptimized: true,
   },
-  // Ensure these match your GitHub repo name
   basePath: isProd ? '/personal-website-2024' : '',
   assetPrefix: isProd ? '/personal-website-2024/' : '',
-  
-  // This can help with serving index.html in subfolders
   trailingSlash: true,
 };
 
