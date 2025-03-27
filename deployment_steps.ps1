8c81b45d14323746732601a2c389ea827995c286
Joel Hempel <joelhempel35@gmail.com>
2025-03-27 00:25:24 -0700

    minor changes

---
+++ deployment_steps.ps1
@@ -0,0 +1,17 @@
+# Stage all changes
+git add .
+
+# Commit changes with a message
+git commit -m "minor changes"
+
+# Push commits to the remote repository
+git push
+
+# Build the project
+npm run build
+
+# Create a .nojekyll file in the out folder
+New-Item -Path "out\.nojekyll" -ItemType File -Force
+
+# Deploy the project
+npm run deploy
