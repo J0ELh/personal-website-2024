2eed1386123b4f968a8effad1a53b46f2601f3f7
Joel Hempel <joelhempel@gmail.com>
2025-09-21 01:46:55 -0700

    Updates

--- src/hooks/useDeploymentContext.tsx
+++
@@ -1,9 +0,0 @@
-// "use client"
-// import { useState, useEffect } from 'react';
-
-// export default function useDeploymentContext() {
-//     const isProd = process.env.NODE_ENV === 'production';
-//     const basePath = isProd ? '/personal-website-2024' : '';
-
-//   return basePath;
-// }
